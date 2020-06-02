055661d76d7cf459d8d303cbba57fa7c9cd37518
sammiefrog <sammiefrog@live.com>
2020-06-02 16:17:43 -0500

    made the models

---
+++ models/index.js
@@ -0,0 +1,3 @@
+modules.exports = {
+    Workout: require("./workout");
+}
---
+++ models/workout.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: { type: Date, require: true },
+    exercises: [
+        {
+            type: { type: String, required: true },
+            name: { type: String, required: true },
+            duration: { type: Number, required: true },
+            weight: Number,
+            reps: Number,
+            sets: Number,
+            distance: Number,
+        },
+    ],
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
