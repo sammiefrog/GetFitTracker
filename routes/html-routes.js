c7a7a4303cd699fdb60e1b185c38df260db933a7
sammiefrog <sammiefrog@live.com>
2020-06-02 16:21:29 -0500

    html routes

---
+++ routes/html-routes.js
@@ -0,0 +1,19 @@
+//importing node packages
+const express = require("express");
+const path = require("path");
+//creating router
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router;
